e21b0b4cf7a560568496ea5cb6e9ef35bef72782
Aryan <aryanverma170372@gmail.com>
2022-08-11 13:36:56 +0530

    feat: search and clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -11,35 +11,47 @@ export const GithubProvider = ({ children }) => {
     users: [],
     loading: false,
   };
-
-  const [state, dispatch] = useReducer(GithubReducer, initialState); //dispatch is the the function name that takes in the type and data (dispatch===action)
-
-  // Get Search results
-  const searchUsers = async () => {
-    setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: { Authorization: `token ${GITHUB_TOKEN}` },
-    });
-    const data = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: data,
-    }); //instead of setUsers we do this
-  };
-
   //   setloading
   const setLoading = () => {
     dispatch({
       type: "SET_LOADING",
     });
   };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState); //dispatch is the the function name that takes in the type and data (dispatch===action)
+
+  // Get Search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` },
+    });
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    }); //instead of setUsers we do this
+  };
+
+  //   clear users from state
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    }); //instead of setUsers we do this
+  };
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
