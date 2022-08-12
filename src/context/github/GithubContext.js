1a399527d8c7e6aab2db88dac8ff58751eb82735
Aryan <aryanverma170372@gmail.com>
2022-08-12 13:06:22 +0530

    feat: creating context & reducer for getUser

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
   //   setloading
@@ -39,6 +40,26 @@ export const GithubProvider = ({ children }) => {
     }); //instead of setUsers we do this
   };
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` },
+    });
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      }); //instead of setUsers we do this
+    }
+  };
+
   //   clear users from state
   const clearUsers = () => {
     dispatch({
@@ -50,7 +71,9 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
+        getUser,
         clearUsers,
       }}
     >
