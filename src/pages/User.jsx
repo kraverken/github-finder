9fcc7b5b2cff2e42b069d2868f785b2641d8588b
Aryan <aryanverma170372@gmail.com>
2022-08-12 23:18:23 +0530

    feat: adding repoList to user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,13 +4,16 @@ import { Link } from "react-router-dom";
 import Spinner from "../components/shared/Spinner";
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
+import RepoList from "../components/repos/RepoList";
 
 function User() {
-  const { getUser, user, loading } = useContext(GithubContext);
+  const { getUser, user, loading, getUserRepos, repos } =
+    useContext(GithubContext);
   const params = useParams();
 
   useEffect(() => {
     getUser(params.login);
+    getUserRepos(params.login);
   }, []);
 
   const {
@@ -157,6 +160,7 @@ function User() {
             </div>
           </div>
         </div>
+        <RepoList repos={repos} />
       </div>
     </>
   );
